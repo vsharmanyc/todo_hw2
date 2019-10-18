9bbc624b1cfa3f5c11581844bd2358ad6d76d91e
vsharmanyc <vsharma.nyc@gmail.com>
2019-10-18 19:07:41 -0400

    css add for up, down, delete

--- src/components/list_screen/ListItemCard.js
+++ src/components/list_screen/ListItemCard.js
@@ -19,9 +19,9 @@ export class ListItemCard extends Component {
                 </div>
 
                 <div className="list_item_card_toolbar">
-                    <img id="" src="images/icons/MoveUp.png" onClick={this.props.moveUp.bind(this, this.props.listItem.key)}></img>
-                    <img id="" src="images/icons/MoveDown.png" onClick={this.props.moveDown.bind(this, this.props.listItem.key)}></img>
-                    <img id="" src="images/icons/Close.png" onClick={this.props.deleteItem.bind(this, this.props.listItem.key)}></img>
+                    <img id="move_up" src="images/icons/MoveUp.png" onClick={this.props.moveUp.bind(this, this.props.listItem.key)}></img>
+                    <img id="move_down" src="images/icons/MoveDown.png" onClick={this.props.moveDown.bind(this, this.props.listItem.key)}></img>
+                    <img id="delete_item" src="images/icons/Close.png" onClick={this.props.deleteItem.bind(this, this.props.listItem.key)}></img>
                 </div>
             </div>
 
